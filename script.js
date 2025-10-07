23593b71f068a66259f8d9bb32dd3a895ba134d0
Taha_Afzali <elfo.dp8787@gmail.com>
2025-10-07 22:43:02 +0330

    traversing

--- script.js
+++ script.js
@@ -156,3 +156,25 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
     });
   }
 });
+//! dom tervesing
+/*
+const h1 = document.querySelector('h1');
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+//////
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+//
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+//?sibling
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+[...h1.parentElement.children].forEach(function (el) {
+  if (el != h1) {
+    el.style.transform = 'scale(0.5)';
+  }
+});
+*/
