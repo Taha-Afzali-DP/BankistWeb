adf8637c9bf9d5315eaa8d4ce53bda34467df34b
Taha_Afzali <elfo.dp8787@gmail.com>
2025-10-12 22:33:09 +0330

    remove opacity in handlerHover func

--- script.js
+++ script.js
@@ -198,7 +198,7 @@ containerTabs.addEventListener('click', function (e) {
     .classList.add('operations__content--active');
 });
 //! Menu fade animation
-const handelHover = function (e, opacity) {
+const handelHover = function (e) {
   if (e.target.classList.contains('nav__link')) {
     const link = e.target;
     const sibling = link.closest('.nav').querySelectorAll('.nav__link');
