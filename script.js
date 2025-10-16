77669e364643bf82529a07360d956db2c6de3017
Taha_Afzali <elfo.dp8787@gmail.com>
2025-10-17 01:03:16 +0330

    add rootMargin

--- script.js
+++ script.js
@@ -256,5 +256,6 @@ const stickyNav = function (entries) {
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
+  rootMargin: -90,
 });
 headerObserver.observe(header);
