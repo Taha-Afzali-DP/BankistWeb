3df8662d30c780da314b29d8d5a1f78afbb3d375
Taha_Afzali <elfo.dp8787@gmail.com>
2025-10-17 00:42:15 +0330

    add headerOBS

--- script.js
+++ script.js
@@ -246,3 +246,4 @@ const observer = new IntersectionObserver(obsCallBack, obsOptions);
 observer.observe(section1);
 */
 //? add sticky widh interasctionObserver API ::
+const headerObserver = new IntersectionObserver();
